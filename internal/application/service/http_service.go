f47791a67ff06695d35a3cb5fafd0dd94dbb261d
Bruno Alliguieri <devalliguieri@gmail.com>
2023-01-17 15:23:55 -0300

    feat: http service created

--- internal/application/service/http_service.go
+++ internal/application/service/http_service.go
@@ -1,9 +1,36 @@
 package service
 
-import "github.com/megalypse/golang-fstresser/internal/domain/usecase"
+import (
+	"github.com/megalypse/golang-fstresser/internal/domain/entity"
+	"github.com/megalypse/golang-fstresser/internal/domain/usecase"
+)
 
 type HttpService struct {
-	GetUsecase      usecase.GetUsecase
-	PostUsecase     usecase.PostUsecase
-	PostformUsecase usecase.PostFormUsecase
+	getUsecase      usecase.GetUsecase
+	postUsecase     usecase.PostUsecase
+	postformUsecase usecase.PostFormUsecase
+}
+
+func (hs HttpService) Get(req entity.Request) entity.Response[entity.Void] {
+	return hs.getUsecase.Get(req)
+}
+
+func (hs HttpService) Post(req entity.Request) entity.Response[entity.Void] {
+	return hs.postUsecase.Post(req)
+}
+
+func (hs HttpService) Postform(req entity.PostformRequest) entity.Response[entity.Void] {
+	return hs.postformUsecase.PostForm(req)
+}
+
+func MakeHttpService(
+	getUsecase usecase.GetUsecase,
+	postUsecase usecase.PostUsecase,
+	postformUsecase usecase.PostFormUsecase,
+) HttpService {
+	return HttpService{
+		getUsecase:      getUsecase,
+		postUsecase:     postUsecase,
+		postformUsecase: postformUsecase,
+	}
 }
